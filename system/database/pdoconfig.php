727d2fdb6a7a3a91d79f5cde1122752120a71e13
denissonwenergy <134405763+denissonwenergy@users.noreply.github.com>
2023-12-22 12:41:50 -0300

    Update pdoconfig.php

--- system/database/pdoconfig.php
+++ system/database/pdoconfig.php
@@ -1,22 +1,8 @@
 <?php
 
-//Produção
-$host='prd-db-wenergy.postgres.database.azure.com';
-$dbname='telemetria';
-$username='wenergydbprd';
-$password='XB53dgai1#UMQnl@';
-$port='5432';
-
 //Homologação
 $host='hml-db-wenergy.postgres.database.azure.com';
 $dbname='telemetria';
 $username='wenergydbhml';
 $password='XB53dgaUMQnl@i1#';
-$port='5432';
-
-//Localhost
-$host='localhost';
-$dbname='telemetria';
-$username='telemetria';
-$password='db@telemetria#23';
 $port='5432';
